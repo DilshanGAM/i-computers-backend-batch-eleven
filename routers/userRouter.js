e34eabcbaf9db098e22eb211699513a0e288f59c
DilshanGAM <malithdilshan27@gmail.com>
2026-06-17 20:46:25 +0530

    Day 25✏️

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createUser, getUser, loginUser } from '../controllers/userController.js';
+import { createUser, getUser, googleLogin, loginUser, updatePassword, updateProfile } from '../controllers/userController.js';
 
 const userRouter = express.Router()
 
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
 userRouter.get("/me", getUser)
+userRouter.put("/",updateProfile)
+userRouter.post("/password",updatePassword)
+userRouter.post("/google-login",googleLogin)
 
 export default userRouter
